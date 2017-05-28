933ac2cab35d4ee00c0f371b5597c5390a9db601
Alexander W Greene <greene.al1991@gmail.com>
2017-05-28 15:32:49 +0000

    Add a form that allows users to  add new campgrounds to the site.

--- README.md
+++ README.md
@@ -9,3 +9,9 @@
 #Layout and Basic Styling
 * Create our header and footer partials
 * Add in Bootstrap
+
+#Createing New Campgours
+* Setup new campground POST route
+* Add in body-parser
+* Setup route to show form
+* Add basic unstyled form
--- app.js
+++ app.js
@@ -1,14 +1,12 @@
 var express = require("express");
 var app = express();
 
+var bodyParser = require("body-parser");
+
+app.use(bodyParser.urlencoded({extended : true}));
 app.set("view engine", "ejs");
 
-app.get("/", function(req, res) {
-    res.render("landing");
-});
-
-app.get("/campgrounds", function(req, res) {
-    var campgrounds = [
+var campgrounds = [
         {
             name : "Salmon Creek",
             image: "https://source.unsplash.com/rU0EP4vdVGY"
@@ -20,8 +18,30 @@ app.get("/campgrounds", function(req, res) {
             image: "https://source.unsplash.com/Aj6mvFNBXAA"
         }
     ];
+    
+
+app.get("/", function(req, res) {
+    res.render("landing");
+});
+
+app.get("/campgrounds", function(req, res) {
     res.render("campgrounds", {campgrounds : campgrounds});
-})
+});
+
+app.post("/campgrounds", function(req, res) {
+    // get data from form and add to campgrounds array
+    var name = req.body.name;
+    var image = req.body.image;
+    var newCampground = {name: name, image: image};
+    campgrounds.push(newCampground);
+    
+    // redirect back to campgrounds page
+    res.redirect("/campgrounds");
+});
+
+app.get("/campgrounds/new", function(req, res){
+   res.render("new.ejs"); 
+});
 
 app.listen(process.env.PORT, process.env.IP, function() {
     console.log("The YelpCamp Server has started!");
