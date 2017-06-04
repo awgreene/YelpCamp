4e0f2b3d64936e08334dcd7c5528986c11be8548
Alexander W Greene <greene.al1991@gmail.com>
2017-06-04 21:43:56 +0000

    Add code to make every template aware of currently signed in user

--- app.js
+++ app.js
@@ -22,6 +22,11 @@ app.use(passport.session());
 passport.use(new LocalStrategy(User.authenticate()));
 passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
+
+app.use(function(req, res, next) {
+    res.locals.currentUser = req.user;
+    next();
+});
     
 app.use(bodyParser.urlencoded({extended : true}));
 app.use(express.static(__dirname + "/public"))
