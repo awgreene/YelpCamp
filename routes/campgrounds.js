3524452a89d7b1612268a2ebe1ab248fbfa17ee4
Alexander W Greene <greene.al1991@gmail.com>
2017-06-15 11:04:45 +0000

    Add dynamic price to each campground

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -21,6 +21,7 @@ router.post("/", middleware.isLoggedIn, function(req, res) {
     // get data from form and add to campgrounds array
     var name = req.body.name;
     var image = req.body.image;
+    var price = req.body.price;
     
     var desc = req.body.description;
     var author = {
@@ -28,7 +29,7 @@ router.post("/", middleware.isLoggedIn, function(req, res) {
         id : req.user._id,
         username : req.user.username
     };
-    var newCampground = {name: name, image: image, description: desc, author: author};
+    var newCampground = {name: name, image: image, description: desc, author: author, price: price};
     
     Campground.create(newCampground, function(err, campground) {
         if(err) {
